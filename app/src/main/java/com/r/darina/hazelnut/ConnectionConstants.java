a538e0e984f0af5b72604828e55d8abc6cb8b97a
ickisIckis <darinafjordur@gmail.com>
2017-01-20 04:57:29 +0300

    Update ConnectionConstants.java

--- app/src/main/java/com/r/darina/hazelnut/ConnectionConstants.java
+++ app/src/main/java/com/r/darina/hazelnut/ConnectionConstants.java
@@ -6,7 +6,7 @@ package com.r.darina.hazelnut;
 
 public class ConnectionConstants {
     final static String BROKER_URI = "tcp://m13.cloudmqtt.com:13514";
-    final static int PORT = 13514;
-    final static String USER = "bcxyzuyr";
-    final static String PASS = "LYqDjOMSRwyx";
+    final static int PORT = ***;
+    final static String USER = "***";
+    final static String PASS = "***";
 }
